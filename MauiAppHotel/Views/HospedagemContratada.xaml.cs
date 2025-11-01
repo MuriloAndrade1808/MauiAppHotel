bfe36609e75f701c6b29aa287f55eeb00ee6b00e
Murilo Andrade <murilowoody@gmail.com>
2025-11-01 14:00:17 -0300

    Adição da Segunda Página e implementação da navegação entre páginas (além da função da lista de quartos)

--- MauiAppHotel/Views/HospedagemContratada.xaml.cs
+++ MauiAppHotel/Views/HospedagemContratada.xaml.cs
@@ -2,8 +2,20 @@ namespace MauiAppHotel.Views;
 
 public partial class HospedagemContratada : ContentPage
 {
-	public HospedagemContratada()
+    public HospedagemContratada()
 	{
 		InitializeComponent();
-	}
+
+    }
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+		try
+		{
+			Navigation.PopAsync();
+		} catch (Exception ex)
+		{
+			DisplayAlert("Ops", ex.Message, "OK");
+		}
+    }
 }
